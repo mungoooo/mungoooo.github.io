47a6c18388c86ec87f06ec7a9c2c5e06f0f545f7
mungoooo <accounts@mungomclaggan.com>
2025-07-20 15:03:42 -0700

    Copied Svelte Stuff

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,46 @@
+<main
+  class="mainminh flex flex-grow flex-col bg-[url(assets/images/backgrounds/backgroundXflip_50.png)] bg-no-repeat pt-[200px] sm:pt-[132px] md:ml-[150px]"
+>
+  <section
+    class="text-secondary mx-auto flex w-full max-w-[1000px] flex-col space-y-3 overflow-y-auto p-6 leading-relaxed [word-spacing:0.1rem] sm:p-12"
+  >
+    <h1 class="text-style-primary font-style-2 mb-[5px] text-[30px]">Get in touch.</h1>
+    <h2>Got questions? Want to work together?</h2>
+    <h2>Feel free to reach out!</h2>
+    <form
+      target="_blank"
+      action="https://formsubmit.co/72367e3aed3d672c9f64e1f533142ac7"
+      method="POST"
+      class="flex flex-col space-y-3"
+    >
+      <div class="flex flex-col md:flex-row">
+        <input
+          type="text"
+          name="name"
+          class="form-control mb-[10px] p-2 md:mr-[10px] md:mb-0 md:w-full"
+          placeholder="Name"
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          class="form-control p-2 md:w-full"
+          placeholder="Email"
+          required
+        />
+      </div>
+      <div class="form-group w-full">
+        <textarea
+          placeholder="Message..."
+          class="form-control max-h-[1000px] min-h-[300px] min-w-full p-2"
+          name="message"
+          rows="10"
+          required
+        ></textarea>
+      </div>
+      <button type="submit">
+        <span class="text-button mt-[-10px] text-[20px]">Submit</span>
+      </button>
+    </form>
+  </section>
+</main>
